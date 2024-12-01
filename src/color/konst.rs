85e0c1d9646db27d3d74380f40e024850d8e53ed
Matthias Beyer <mail@beyermatthias.de>
2024-12-01 15:30:27 +0100

    Add ConstNColor for returning the same color sequence again and again
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/color/konst.rs
+++ src/color/konst.rs
@@ -17,3 +17,38 @@ impl ConstColor {
         Self { color }
     }
 }
+
+pub struct ConstNColor<const N: usize> {
+    color: [RGB8; N],
+    idx: usize,
+}
+
+impl<const N: usize> ConstNColor<N> {
+    pub const fn from_slice(color: [RGB8; N]) -> Self {
+        Self { color, idx: 0 }
+    }
+
+    pub fn from_provider<CP>(mut provider: CP) -> Self
+    where
+        CP: Provider,
+    {
+        let mut color = [RGB8::default(); N];
+
+        for idx in 0..N {
+            color[idx] = provider.provide_next();
+        }
+
+        Self { color, idx: 0 }
+    }
+}
+
+impl<const N: usize> Provider for ConstNColor<N> {
+    fn provide_next(&mut self) -> smart_leds::RGB8 {
+        let current = self.color[self.idx];
+        self.idx += 1;
+        if self.idx == N {
+            self.idx = 0;
+        }
+        current
+    }
+}
